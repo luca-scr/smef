5b8139588b304874163d1889d18ec05d211e9a8c
luca-scr <luca.scrucca@unipg.it>
2019-09-03 14:12:29 +0200

    initial commit

---
+++ R/confint.maxLik.R
@@ -0,0 +1,27 @@
+# Wald conﬁdence intervals
+# based on asymptotic Gaussian distribution of MLE
+
+require(maxLik)
+
+confint.maxLik <- function(object, parm, level = 0.95, ...)
+{
+  cf <- coef(object)
+  if(missing(parm)) 
+    parm <- seq(cf)
+  pnames <- names(cf)
+  if(is.null(pnames)) 
+    pnames <- parm
+  else if(is.numeric(parm)) 
+         parm <- pnames[parm]
+  a <- (1 - level)/2
+  a <- c(a, 1 - a)
+  pct <- stats:::format.perc(a, 3)
+  q <- qnorm(a)
+  ci <- array(NA, dim = c(length(parm), 2L), 
+              dimnames = list(parm, pct))
+  se <- sqrt(diag(vcov(object)))[parm]
+  ci[] <- cf[parm] + se %o% q
+  return(ci)
+}
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# smef: Statistical Methods for Economy and Finance
+
+## Description
+
+An R package accompanying the courses *Statistical Methods For Finance* (Mod. I) and *Statistical Learning and Data Mining* (Mod. II).
+
+## Installation
+
+```{r}
+# install.packages("devtools")
+devtools::install_github("luca-scr/smef")
+```
