f5a28e5f312238945cd13977c08eed38b4db2899
Luca Scrucca <luca.scrucca@gmail.com>
2021-10-04 09:42:55 +0200

    Updated plotImage() man and code

--- R/plotImage.R
+++ R/plotImage.R
@@ -1,20 +1,23 @@
 #' @name plotImage
 #' @aliases plotImage
 #' 
-#' @title Wald confidence intervals for Maximum Likelihood Estimates
+#' @title Plot image data
 #' 
-#' @description Wald confidence intervals based on asymptotic Gaussian
-#' distribution of MLE.
+#' @description Plot a matrix representing a Grey-scale image or a 
+#' 3-dimensional array representing a RGB image.
 #' 
-#' @param x a matrix of data values for grey-scale images and a 3-dimensional array for RGB images.
-#' @param max the maximum value of RGB color scale.
+#' @param x a matrix of data values for Grey-scale images and a 
+#' 3-dimensional array for RGB images.
+#' @param max the maximum value of Grey/RGB colour scale (default 255 
+#' for integer values in the range [0, 255]; another common value is 1 
+#' for values in the range [0, 1].
 #' @param mar the margin used in the base R graphic. 
-#' @param \dots additional arguments to be passed to the low level functions.
+#' @param \dots additional arguments to be passed to the low level 
+#' functions.
 #' 
 #' @importFrom graphics par rasterImage
 #' @importFrom grDevices as.raster
 #' 
-#' @importFrom stats coef fivenum median na.omit qnorm sd var vcov
 #' @rdname plotImage
 #' @export
 
@@ -25,6 +28,8 @@ plotImage <- function(x, max = 255, mar = c(0,0,0,0), ...)
   x <- as.raster(x, max = max)
   h <- nrow(x)
   w <- ncol(x)
+  oldpar <- par(no.readonly = TRUE)
+  on.exit(par(oldpar))
   par(mar = mar)
   plot(0, 0, type = "n", xlim = c(1, w), ylim = c(1, h),
        axes = FALSE, ann = FALSE, asp = 1)
